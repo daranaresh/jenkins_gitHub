90911b82da5c97d30715362e888a656985bcf4a8
daranaresh <naresh.dara@exelaonline.com>
2019-10-17 19:13:29 +0530

    ******

---
+++ Hello01.java
@@ -0,0 +1,12 @@
+package first_project_git;
+
+public class Hello01 {
+	
+	public static void main(String[] args)
+	
+	{
+		for(int i=1;i<=10;i++)
+	{
+			System.out.println("testing first jenkins_github"+i);}
+	}
+}
